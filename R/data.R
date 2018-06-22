b83a5c772d2ee2560415cf23f0c8210540558350
Marc Choisy <marc.choisy@ird.fr>
2018-06-22 12:14:18 +0700

    Initial commit

--- R/data.R
+++ R/data.R
@@ -4,24 +4,28 @@
 #' 2010 to 2015.
 #'
 #' The provinces' definition used is before the merging of the provinces of Ha
-#' Tay and Ha Noi. Note also that we consider the TB hospital separately. Unless
-#' explictly specified otherwise, TB cases are pulmonary ones. Finally, note that
-#' the columns 4 to 11 are exclusive, meaning that one case cannot below to
-#' different columns at the same time.
+#' Tay and Ha Noi. Note also that we consider the TB hospital separately. Note
+#' that the columns 4 to 11 are exclusive, meaning that one case cannot below to
+#' different columns at the same time. The tree below illustrate the structure
+#' of the data:
+#'
+#' \figure{tree.png}{}
+#'
 #'
 #' @format A data frame with 1,560 rows and 11 variables:
 #' \itemize{
+#'    \item \code{province}: province name (or hospital)
 #'    \item \code{year}: year of case
 #'    \item \code{quarter}: quarter of case
-#'    \item \code{province}: province name
+#'    \item \code{nptb}: non-pulmonary tuberculosis
+#'    \item \code{neg_afb}: AFB-negative
+#'    \item \code{neg_afb_nptb}: AFB-negative corresponding to non-pulmonary tuberculosis
 #'    \item \code{pos_afb_new}: AFB-positive corresponding to a new infection
 #'    \item \code{pos_afb_relapse}: AFB-positive corresponding to a relapse
 #'    \item \code{pos_afb_fail}: AFB-positive corresponding to a treatment failure
 #'    \item \code{pos_afb_retreatment}: AFB-positive corresponding to a retreatment
 #'    \item \code{pos_afb_other}: AFB-positive corresponding to other cases that the 4 listed above
-#'    \item \code{neg_afb}: AFB-negative
-#'    \item \code{nptb}: non-pulmonary tuberculosis
-#'    \item \code{neg_afb_nptb}: AFB-negative corresponding to non-pulmonary tuberculosis
 #' }
 #' @source National TB Programme of Vietnam.
+#' @export
 "tbvn"
